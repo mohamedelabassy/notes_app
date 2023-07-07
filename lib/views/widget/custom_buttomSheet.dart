7bd5e2f23fa50ea7fe4504a8c7481e1a46f45a1e
mohamedelabassy <mohamedelabasy144@gmail.com>
2023-07-07 23:23:49 +0300

    create notes state

--- lib/views/widget/custom_buttomSheet.dart
+++ lib/views/widget/custom_buttomSheet.dart
@@ -30,7 +30,10 @@ class AddNoteBootomSheet extends StatelessWidget {
             return AbsorbPointer(
               absorbing: state is AddNoteLoading ? true : false,
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
+                padding: EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
                 child: SingleChildScrollView(child: AddNoteFormState()),
               ),
             );
