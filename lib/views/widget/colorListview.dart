8edc1d1a2fb658e8ee2a4ddbaa108cc2bf739932
mohamedelabassy <mohamedelabasy144@gmail.com>
2023-07-15 21:50:07 +0300

    colors list view part 2

--- lib/views/widget/colorListview.dart
+++ lib/views/widget/colorListview.dart
@@ -1,29 +1,66 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.isActiv, required this.color});
+  final bool isActiv;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.blue,
-      radius: 38,
-    );
+    return isActiv
+        ? CircleAvatar(
+            radius: 38,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 36,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 38,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
-  const ColorsListView({super.key});
+class ColorsListView extends StatefulWidget {
+  const ColorsListView({
+    super.key,
+  });
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xff1c5253),
+    Color(0xff544b3d),
+    Color(0xff3d348b),
+    Color(0xffe03616),
+    Color(0xff20a39e)
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActiv: currentIndex == index,
+              ),
+            ),
+          );
         },
       ),
     );
